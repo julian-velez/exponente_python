8df47029b9be507accb88c3013a3034b3f1c09b4
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-11-07 15:07:09 -0500

    exponente_python
    exponente_python

---
+++ exponente/main.py
@@ -0,0 +1,8 @@
+# Importamos el módulo 'math', que contiene funciones matemáticas avanzadas
+import math
+
+# Usamos la función exp() del módulo math
+# math.exp(x) calcula el valor de e elevado a la potencia x
+# Donde 'e' es el número de Euler (~2.71828)
+# En este caso, exp(2) = e^2 ≈ 7.38905609893065
+print(math.exp(2))  # Muestra en pantalla el resultado de e^2
